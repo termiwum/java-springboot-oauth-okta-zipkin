8bfe239987746a1014b82d9982582179d0854c0f
termiwum <oscarburdiles@gmail.com>
2025-07-23 23:12:50 -0400

    Primer commit del proyecto cloud-gateway

---
+++ src/main/java/com/termiwum/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,35 @@
+package com.termiwum.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.just("userKey");
+	}
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> {
+			// Default configuration for all circuit breakers
+			return new Resilience4JConfigBuilder(id).circuitBreakerConfig(
+					CircuitBreakerConfig.ofDefaults()).build();
+		});
+	}
+
+}
