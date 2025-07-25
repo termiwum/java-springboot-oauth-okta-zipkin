a6172448af8053d80b00fa5dd58c823980465ba9
termiwum <oscarburdiles@gmail.com>
2025-07-25 11:26:08 -0400

    refactor

--- src/main/java/com/termiwum/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/termiwum/productservice/service/ProductServiceImpl.java
@@ -1,7 +1,6 @@
 package com.termiwum.productservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.termiwum.productservice.entity.Product;
