aea0b013fc7ea3ae2a3f77df430cf4af7b5e607b
termiwum <oscarburdiles@gmail.com>
2025-07-25 15:51:07 -0400

    refactor - test without oauth

--- src/main/java/com/termiwum/paymentservice/security/WebSecurityConfig.java
+++ src/main/java/com/termiwum/paymentservice/security/WebSecurityConfig.java
@@ -2,23 +2,23 @@ package com.termiwum.paymentservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+// import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+// @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorize -> authorize
-                .antMatchers("/payments/**")
-                .hasAuthority("SCOPE_internal")
-                .anyRequest()
-                .authenticated())
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-        return http.build();
-    }
+    // @Bean
+    // SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    // http.authorizeRequests(authorize -> authorize
+    // .antMatchers("/payments/**")
+    // .hasAuthority("SCOPE_internal")
+    // .anyRequest()
+    // .authenticated())
+    // .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    // return http.build();
+    // }
 }
