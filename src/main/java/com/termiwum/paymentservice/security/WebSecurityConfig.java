3955c37f0c60940d6605cb97746c37b3c292aca1
termiwum <oscarburdiles@gmail.com>
2025-07-30 12:07:09 -0400

    refactor auth

--- src/main/java/com/termiwum/paymentservice/security/WebSecurityConfig.java
+++ src/main/java/com/termiwum/paymentservice/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.termiwum.paymentservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
     @Bean
